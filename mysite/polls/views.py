5676dd7f90e9f5bf9ccebed1bf242b3ee06ab803
yuminghe <1341482598@qq.com>
2022-10-16 00:27:18 +0800

    django docs tutorial05

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -12,7 +13,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -37,4 +38,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
+        # HttpResponseRedirect作用：从视图函数跳到另一个视图函数
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
