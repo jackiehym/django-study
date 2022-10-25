78f18d6ca6cccae1186a11d1fd893f3a6e6a1dde
yuminghe <1341482598@qq.com>
2022-10-25 23:20:38 +0800

    HelloDjangoBlog-day03

--- HelloDjangoBlog/blog/admin.py
+++ HelloDjangoBlog/blog/admin.py
@@ -3,6 +3,11 @@ from .models import Category, Tag, Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'create_time', 'modified_time', 'category', 'author']
+    fields = ['title', 'body', 'excerpt', 'category', 'tags']
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
 
 
 # Register your models here.
--- HelloDjangoBlog/blog/models.py
+++ HelloDjangoBlog/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
 class Category(models.Model):
@@ -33,7 +35,7 @@ class Post(models.Model):
     title = models.CharField('标题', max_length=70)
     # 字符串较长的用Text
     body = models.TextField('正文', )
-    create_time = models.DateTimeField('创建时间', )
+    create_time = models.DateTimeField('创建时间', default=timezone.now)
     modified_time = models.DateTimeField('修改时间', )
     # 摘要
     excerpt = models.CharField('摘要', max_length=200, blank=True)
@@ -47,6 +49,12 @@ class Post(models.Model):
     class Meta:
         verbose_name = '文章'
         verbose_name_plural = verbose_name
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.modified_time = timezone.now()
+        super().save(force_insert=False, force_update=False, using=None,
+                     update_fields=None)
 
     def __str__(self):
         return self.title
