978dab4fec12df09d01a5079e2707d8eabdf44eb
yuminghe <1341482598@qq.com>
2022-10-26 22:51:31 +0800

    HelloDjangoBlog-day04

--- HelloDjangoBlog/blog/views.py
+++ HelloDjangoBlog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 # Create your views here.
@@ -9,4 +9,8 @@ def index(request):
     return render(request, 'blog/index.html', context={
         'post_list': post_list,
     })
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/single.html', context={'post': post})
 
