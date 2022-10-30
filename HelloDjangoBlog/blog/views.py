f72493cd6d549ba620baa455b2684dcf95e6909e
yuminghe <1341482598@qq.com>
2022-10-30 22:36:25 +0800

    HelloDjangoBlog-day07

--- HelloDjangoBlog/blog/views.py
+++ HelloDjangoBlog/blog/views.py
@@ -5,7 +5,7 @@ from markdown.extensions.toc import TocExtension
 from django.shortcuts import render, get_object_or_404
 from django.utils.text import slugify
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category, Tag
 
 # Create your views here.
 
@@ -28,4 +28,20 @@ def detail(request, pk):
     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
     post.toc = m.group(1) if m is not None else ''
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(create_time__year=year,
+                                    create_time__month=month
+                                    ).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def tag(request, pk):
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
 
