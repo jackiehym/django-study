cf4a1a797aa7bd51ee0ffc41f09086e6ff3cf1ca
yuminghe <1341482598@qq.com>
2022-11-07 23:33:33 +0800

    HelloDjangoBlog-day10

--- HelloDjangoBlog/blog/views.py
+++ HelloDjangoBlog/blog/views.py
@@ -12,6 +12,7 @@ class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
+    paginate_by = 10
 
 
 class PostDetailView(DetailView):
